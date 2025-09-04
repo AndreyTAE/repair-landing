e6f43bc4f55ad90f11396fcb1da23b2c5fc9a5e4
Andrey <troeglazoofcar@gmail.com>
2025-09-04 09:26:13 +0300

    final yandex45

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const elements = document.querySelectorAll(".animate");
 
   const onScroll = () => {
-    const triggerBottom = window.innerHeight * 0.85;
+    const triggerBottom = window.innerHeight * 0.9;
     elements.forEach(el => {
       const rect = el.getBoundingClientRect();
       if (rect.top < triggerBottom) {
